333c656c581a57478c5cfa4082809c129e22054c
captainIT <captainlin205@gmail.com>
2024-03-27 11:40:41 +0800

    fix test

--- src/components/layout/components/SideBar.vue
+++ src/components/layout/components/SideBar.vue
@@ -1,28 +1,31 @@
 <template>
   <div class="sideBar">
-    <MenuItem v-for="n in menus" v-bind="n"  v-model:selectedMenu="selectedMenu" ></MenuItem>
-
+    <MenuItem
+      v-for="n in menus"
+      v-bind="n"
+      v-model:selectedMenu="selectedMenu"
+    ></MenuItem>
   </div>
 </template>
 
 <script setup>
-
 // Data: '数据',
 //   Labeling: '标注',
 //   Model: '模型',
 
 import { ref } from "vue";
 
-const menus=[{ selected: false, name: "Data", route: "/data",testKey:1 }, {
-  selected: false,
-  name: "Labeling",
-  route: "/labeling",testKey:2,
+const menus = [
+  { selected: false, name: "Data", route: "/data", testKey: 1 },
+  //   , {
+  //   selected: false,
+  //   name: "Labeling",
+  //   route: "/labeling",testKey:2,
+  //
+  // }, { selected: true, name: "Model", route: "/model",testKey:3 }
+];
 
-}, { selected: true, name: "Model", route: "/model",testKey:3 }]
-
-const selectedMenu=ref(1)
-
-
+const selectedMenu = ref(1);
 </script>
 
 <style scoped lang="scss">
